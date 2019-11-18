e731b6551a1c136a24890563da74844de487b6b9
GabrielCPDev <gabrielcpcontato@outlook.com>
2019-11-18 12:45:35 -0300

    Aprimorando

--- src/domain/PalavraTeste.java
+++ src/domain/PalavraTeste.java
@@ -1,5 +1,23 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PalavraTeste {
 
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String palavra = "arara";
+		String palavraEmbaralhada = "arraa";
+		
+		final String[] palavraLista = palavra.split("");
+		Arrays.sort(palavraLista);
+		
+		final String[] palavraEmbaralhadaLista = palavraEmbaralhada.split("");
+		Arrays.sort(palavraEmbaralhadaLista);
+		
+		System.out.println(Arrays.deepEquals(palavraLista, palavraEmbaralhadaLista));
+		
+		sc.close();
+	}
 }
